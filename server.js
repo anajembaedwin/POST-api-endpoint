d42d3d442f6818916b7da05cbbe0a658612c51b0
iSommie <anajembaedwin@gmail.com>
2022-11-06 23:04:10 +0100

    updated post return value

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // load up the express framework
 const express = require("express");
 
-
 // create an instance of express to serve our end points
 const app = express();
 
@@ -16,16 +15,6 @@ app.use(cors({
 }));
 
 
-
-// app.get("/", function (req, res) {
-//     res.send("Hello World");
-// })
-
-// app.post("/", function (req, res) {
-
-//   res.send("This is a post Request");
-// })
-
 app.get("/", (req, res) => {
     //fetch all users
     //send user array as response to the client
@@ -33,13 +22,13 @@ app.get("/", (req, res) => {
 })
 
 app.post("/", (req, res) => {
-    const { operator_type } = req.body;
-    let getNumber = operator_type.match(/\d+/g)
+    const { operation_type } = req.body;
+    let getNumber = operation_type.match(/\d+/g)
 
     let x = Number(getNumber[0]);
     let y = Number(getNumber[1]);
     let result;
-    let getOperation = operator_type.match(/(add?[^\s]+)|(addition?[^\s]+)|(subtract?[^\s]+)|(subtraction?[^\s]+)|(multiply?[^\s]+)|(multiplication?[^\s]+)/gi)
+    let getOperation = operation_type.match(/(add?[^\s]+)|(addition?[^\s]+)|(subtract?[^\s]+)|(subtraction?[^\s]+)|(multiply?[^\s]+)|(multiplication?[^\s]+)/gi)
     let operation = getOperation[0]
   
     switch (operation) {
@@ -47,11 +36,23 @@ app.post("/", (req, res) => {
             result = x + y;
             break;
         }
+        case "Add": {
+            result = x + y;
+            break;
+        }
         case "addition": {
             result = x + y;
             break;
         }
+        case "Addition": {
+            result = x + y;
+            break;
+        }
         case "subtract": {
+            result = x - y;
+            break;
+        }
+        case "Subtract": {
             result = x - y;
             break;
         }
@@ -73,7 +74,7 @@ app.post("/", (req, res) => {
     }
 
 
-    users.push(req.body);
+    
 
     let stringedData = JSON.stringify(users, null, 2)
     fs.writeFile("users.json", stringedData, function(err) {
@@ -82,8 +83,11 @@ app.post("/", (req, res) => {
         }
     })
 
-    return res.json({"slackUsername": "iSommie", "result": result,  "operator_type": operation})
+    let user = {"slackUsername": "iSommie", "result": result,  "operation_type": operation}
+    users.push(user);
+    return res.send(user)
 })
+
 
 const PORT = process.env.PORT || 3000;
 
