37c845e12e72af8e085f968ab94a74cdb1e6b5ea
anajembaedwin <anajembaedwin@gmail.com>
2022-11-06 03:01:03 +0100

    updated post method

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # POST-api-endpoint
 A NodeJS (POST) API Endpoint that performs a simple arithmetic operation and returns a response with the result of the operation and my slack username
+
+# HTTP operations
+These operations are integral to the HTTP protocol and represent essential create, read, update, and delete (CRUD) functionality, although not in a clean one-to-one manner:
+
+POST (create a resource or generally provide data)
+GET (retrieve an index of resources or an individual resource)
+PUT (create or replace a resource)
+PATCH (update/modify a resource)
+DELETE (remove a resource)
+
+# Dependencies
+npm init
+npm install express
+npm install --save-dev nodemon 
+npm install --save cors
+npm install --save body-parser
---
+++ server.js
@@ -0,0 +1,75 @@
+// load up the express framework
+const express = require("express");
+
+
+// create an instance of express to serve our end points
+const app = express();
+
+const cors = require('cors');
+const users = require('./users.json')
+const fs = require('fs');
+
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded());
+
+app.use(cors({
+    methods: ['POST'],
+    origin: '*'
+}));
+
+
+
+app.get("/", function (req, res) {
+    res.send("Hello World");
+})
+
+app.post("/", function (req, res) {
+
+  res.send("This is a post Request");
+})
+
+app.get("/users", (req, res) => {
+    //fetch all users
+    //send user array as response to the client
+    return res.json(users)
+})
+
+app.post("/users", (req, res) => {
+    const { operation_type, x, y } = req.body;
+
+    let result;
+    let operation = operation_type;
+    switch (operation_type) {
+        case "add": {
+            result = x + y;
+            break;
+        }
+        case "subtract": {
+            result = x - y;
+            break;
+        }
+        case "multiply": {
+            result = x * y;
+            break;
+        }
+        default: {
+            result = "No value found";
+        }
+    }
+
+    users.push(req.body);
+
+    let stringedData = JSON.stringify(users, null, 2)
+    fs.writeFile("users.json", stringedData, function(err) {
+        if (err) {
+            return res.status(500).json({message: err})
+        }
+    })
+
+    return res.status(200).json({"slackUsername": "iSommie", "result": result,  "operation_type": operation})
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
